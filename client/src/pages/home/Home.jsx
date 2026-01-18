6eeb8c43a7bcbf1177ab8c8b37853c897a3d0621
John Michael <bacasno.johnmichael@gmail.com>
2026-01-18 20:32:46 +0800

    feat(client): add necessary components in home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from './Banner.jsx';
+import TopSellers from './TopSellers.jsx';
+import Recommended from './Recommended.jsx';
+import News from './News.jsx';
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <TopSellers />
+      <Recommended />
+      <News />
     </>
   )
 }
