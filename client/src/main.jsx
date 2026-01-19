7e75b1edb1e9a0c723e06d132ea238b429b0e093
John Michael <bacasno.johnmichael@gmail.com>
2026-01-19 20:48:11 +0800

    feat(client): add redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import store from './redux/store.js';
+import { Provider } from 'react-redux';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 import App from './App.jsx';
 import router from './routers/router.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
 )
